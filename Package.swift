471e06f2f5803c3cd2d585451e11d514b6b763b5
Umang <umang.multiqos@gmail.com>
2024-07-04 10:13:29 +0530

    - add privacy manifest

--- Package.swift
+++ Package.swift
@@ -17,6 +17,12 @@ let package = Package(
             name: "openssl",
             url: "https://github.com/passepartoutvpn/openssl-apple/releases/download/3.2.107/openssl.xcframework.zip",
             checksum: "6aa5b917eb03487978b5d6ab1c019127af4f67568bd1f793c5b58cf786a49399"),
+        
+            .target(
+                name: "openssl-privacy",
+                path: "Sources",
+                resources: [.process("Sources/PrivacyInfo.xcprivacy")]
+            )
 
         // local development
 //        .binaryTarget(
